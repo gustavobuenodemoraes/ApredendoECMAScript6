deda9c5848f652bda33650dab8e43180b7d4f682
GUM <gustavo@akatecnologia.com>
2019-10-30 13:22:42 -0300

    Async/await

--- src/main.js
+++ src/main.js
@@ -1,1 +1,22 @@
-alert('Opa');
+const minhaPromise = () => new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve('ok')
+    }, 2000);
+});
+
+// async  function executePromise() {
+//     const reponse = await minhaPromise();
+
+//     console.log(reponse);
+//     console.log(reponse);
+//     console.log(reponse);
+
+// }
+
+// const executePromise = async () => {
+//     console.log(await minhaPromise());
+//     console.log(await minhaPromise());
+//     console.log(await minhaPromise());
+// }
+
+// executePromise();
