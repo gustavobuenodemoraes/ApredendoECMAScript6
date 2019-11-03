33ec74cb0e3de1bfb0470ee019f16e8654f3b572
GUM <gustavo@akatecnologia.com>
2019-11-03 15:31:20 -0300

    Concuido!

--- src/main.js
+++ src/main.js
@@ -1,10 +1,91 @@
-import axios from "axios";
+import api from './api';
+class App {
+    constructor() {
+        this.repositories = [];
 
-class Api {
-    static async getUserInfo(username){
-        const response = await axios.get(`http://api.github.com/users/${username}`);
-        console.log(response);
+        this.formEl = document.getElementById('repo-form');
+        this.inputEl = document.querySelector('input[name=repository]');
+        this.listEl = document.getElementById('repo-list');
+
+        this.registerHandlers();
+    }
+
+    registerHandlers() {
+        this.formEl.onsubmit = event => this.addRepository(event);
+    }
+
+    setLoading(loading = true) {
+        if (loading === true) {
+            let loadingEl = document.createElement('span');
+            loadingEl.appendChild(document.createTextNode('Carregando...'));
+            loadingEl.setAttribute('id', 'loading');
+
+            this.formEl.appendChild(loadingEl);
+        } else {
+            document.getElementById('loading').remove();
+        }
+    }
+
+    async addRepository(event) {
+        event.preventDefault();
+
+        const repoInput = this.inputEl.value;
+
+        if (repoInput.lenght === 0) return;
+
+        this.setLoading();
+
+        try {
+            const response = await api.get(`/repos/${repoInput}`);
+
+            const { name, description, html_url, owner: { avatar_url } } = response.data;
+
+            this.repositories.push({
+                name,
+                description,
+                avatar_url,
+                html_url
+            });
+
+            this.render();
+            this.inputEl.value = '';
+        } catch (error) {
+            alert('Repositorio não existe!');
+        }
+
+        this.setLoading(false);
+
+    }
+
+    render() {
+        this.listEl.innerHTML = "";
+
+        this.repositories.forEach(repo => {
+            let imgEl = document.createElement('img');
+            let titleEl = document.createElement('strong');
+            let descriptionEl = document.createElement('p');
+            let linkEl = document.createElement('a');
+
+
+            imgEl.setAttribute('src', repo.avatar_url);
+            imgEl.className = 'rounded';
+            titleEl.appendChild(document.createTextNode(repo.name));
+            descriptionEl.appendChild(document.createTextNode(repo.description));
+            linkEl.setAttribute('target', '_blank');
+            linkEl.appendChild(document.createTextNode('Acessar'));
+            linkEl.setAttribute('href', repo.html_url);
+
+            let listItemEl = document.createElement('li');
+            listItemEl.className = 'list-group-item';
+            listItemEl.appendChild(imgEl);
+            listItemEl.appendChild(titleEl);
+            listItemEl.appendChild(descriptionEl);
+            listItemEl.appendChild(linkEl);
+
+            this.listEl.appendChild(listItemEl);
+
+        })
     }
 }
 
-Api.getUserInfo('gustavobuenodemoraes');
+new App();
